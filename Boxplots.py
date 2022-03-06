7ec027bbaa0d87c27d9356f83bf8bef0991a946e
Elonisme <3120190971352@stu.xhu.edu.cn>
2022-03-06 11:35:37 +0800

    main

--- Boxplots.py
+++ Boxplots.py
@@ -4,6 +4,14 @@ import matplotlib.pyplot as plt
 class Boxplots:
     def plot(selfs):
         date = [-35,10,20,30,40,50,60,106]
-    filerprops = {'marker':'o','markerfacecolor':'red','color':'black'}
-    plt.grid(True, linestyle = "-.",color = "black",linewidth = "0.4")
-    plt.show()
+        filerprops = {'marker':'o','markerfacecolor':'red','color':'black'}
+        plt.grid(True, linestyle = "-.",color = "black",linewidth = "0.4")
+        plt.boxplot(date,notch = False, flierprops = filerprops)
+        plt.show()
+
+
+if __name__ == '__main__':
+    B =Boxplots()
+    B.plot()
+
+    

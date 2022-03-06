7a7a70b7285c3778576fa61a274b92f94c900b27
Elonisme <3120190971352@stu.xhu.edu.cn>
2022-03-06 11:26:09 +0800

    mian

--- Boxplots.py
+++ Boxplots.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as plt
 
+
 class Boxplots:
     def plot(selfs):
         date = [-35,10,20,30,40,50,60,106]
+    filerprops = {'marker':'o','markerfacecolor':'red','color':'black'}
+    plt.grid(True, linestyle = "-.",color = "black",linewidth = "0.4")
+    plt.show()
